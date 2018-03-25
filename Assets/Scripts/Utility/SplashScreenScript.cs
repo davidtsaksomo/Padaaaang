467cb0aa365d9c3f26ee3b62ddf318ddf1e262c5
SpiritCK <kevinjonathank97@gmail.com>
2018-03-25 19:53:46 +0700

    Add splash screen

---
+++ Assets/Scripts/Utility/SplashScreenScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SplashScreenScript : MonoBehaviour {
+	public float delay;
+	public RectTransform logo;
+	public float scale;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (LoadTitleAfterDelay (delay));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		logo.sizeDelta += new Vector2(logo.sizeDelta.x * scale * Time.deltaTime,
+			logo.sizeDelta.y * scale * Time.deltaTime);
+	}
+
+	IEnumerator LoadTitleAfterDelay(float delay)
+	{
+		yield return new WaitForSeconds(delay);
+		Application.LoadLevel("Title");
+	}
+}
