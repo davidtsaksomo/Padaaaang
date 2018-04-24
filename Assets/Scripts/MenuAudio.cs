47853f7a4da9025d44815606224e15d505096a70
Lazuardi Firdaus <lazuardifirdaus369@yahoo.com>
2018-04-24 22:40:19 +0700

    Added Menu Audio prefab and scripts

---
+++ Assets/Scripts/MenuAudio.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuAudio : MonoBehaviour {
+	private AudioSource sounds;
+	public AudioClip shakingGlass;
+	public AudioClip slidingSpoon;
+	public AudioClip slidingSpoonReverse;
+	public AudioClip tappingChopsticks;
+	public AudioClip tappingSpoon;
+	private static bool created = false;
+
+	void Awake(){
+		if (!created) {
+			DontDestroyOnLoad (this.gameObject);
+			created = true;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		sounds = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void shakeGlass(){
+		playClip (shakingGlass);
+	}
+
+	public void slideSpoon(){
+		playClip (slidingSpoon);
+	}
+
+	public void slideSpoonReverse(){
+		playClip (slidingSpoonReverse);
+	}
+
+	public void tapChopsticks(){
+		playClip (tappingChopsticks);
+	}
+
+	public void tapSpoon(){
+		playClip (tappingSpoon);
+	}
+
+	void playClip(AudioClip clip){
+		sounds.time = 0f;
+		sounds.clip = clip;
+		sounds.Play ();
+	}
+}
